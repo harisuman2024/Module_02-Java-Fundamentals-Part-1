70d381dfc8606d2e93eff80cd01f601b9de95f14
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2024-09-13 17:23:29 +0530

    Test Practice Code
    Test Practice Code !!!

---
+++ Test20.java
@@ -0,0 +1,7 @@
+class Test20 {
+    String email;
+
+    public static void main(String as[]) {
+        System.out.println(email);
+    }
+}
---
+++ Test24.java
@@ -0,0 +1,7 @@
+class Test24 {
+    static char val;
+
+    public static void main(String jlc[]) {
+        System.out.println(val == 0);
+    }
+}
